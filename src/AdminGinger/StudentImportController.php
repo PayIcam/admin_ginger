0d55e113d9c0a8e6c18ee35869c224be9ac7ad60
Aymeric <aymeric.robinne@2017.icam.fr>
2017-01-24 09:02:15 +0100

    Add files via upload
    Erreur de fichier uploadé précédemment

--- src/AdminGinger/StudentImportController.php
+++ src/AdminGinger/StudentImportController.php
@@ -1,21 +1,15 @@
 <?php
-
 namespace AdminGinger;
-
 class StudentImportController {
-
     public $curDbStudents;
     public $newStudents;
-
     function __construct($uploadfile) {
         global $DB, $settings;
-
         if (file_exists($uploadfile)) {
             $fp = fopen($uploadfile, "r");
             $keys = [];
             $this->newStudents = [];
             $this->newStudentsById = [];
-
             while (!feof($fp)) {
                 $row = utf8_encode(trim(str_replace("\n", "", fgets($fp, 8192))));
                 if (empty($row))
@@ -29,10 +23,8 @@ class StudentImportController {
                     $field = [];
                     foreach ($res as $k => $v)
                         $field[$keys[$k]] = $v;
-
                     if (empty($field['email']))
                         continue;
-
                     // public static $filieres = array(
                     //     'Intégré'    => 'Intégré',
                     //     'Apprentissage'   => 'Apprentissage',
@@ -40,7 +32,6 @@ class StudentImportController {
                     //     'mgf'        => 'Master Génie Féroviaire',
                     //     'f_continue' => 'Formation Continue'
                     // );
-
                     if (substr($field['filiere_site'], 0, 8) == 'ICAM Int')
                         $filiere = 'Intégré';
                     else if (substr($field['filiere_site'], -2) == 'FC')
@@ -49,7 +40,6 @@ class StudentImportController {
                         $filiere = 'Apprentissage';
                     else
                         $filiere = 'Permanent';
-
                     if (strpos($field['anniversaire'], '/') > -1) {
                         $anniv = explode('/', $field['anniversaire']);
                         $anniv = $anniv[2].'-'.$anniv[1].'-'.$anniv[0];
@@ -68,24 +58,19 @@ class StudentImportController {
                         "sexe" => $field['isFille']=='VRAI'? 2 : 1,
                         "img_link" => $field['img_link']
                     ];
-
                     $this->newStudents[$user['mail']] = $user;
                     $this->newStudentsById[$user['id_icam']] = $user;
                 }
             }
-
             fclose($fp);
             unlink($uploadfile);
-
             $this->curDbStudents = $DB->query("SELECT * FROM users");
-
             $this->usersPerGroup = [
                 'nouveau' => [],
                 'update' => [],
                 'updateRedoublants' => [],
                 'pasMaj' => []
             ];
-
             foreach ($this->curDbStudents as $userDB) {
                 // 'id_icam'
                 // 'login'
@@ -100,7 +85,6 @@ class StudentImportController {
                 // 'naissance'
                 // 'sexe'
                 // 'img_link'
-
                 $idIcam = substr($userDB['img_link'], -5);
                 if (!empty($this->newStudents[$userDB['mail']])) { // MAJ
                     $updateUsr = $this->newStudents[$userDB['mail']];
@@ -127,7 +111,6 @@ class StudentImportController {
             }
             $this->usersPerGroup['nouveau'] = $this->newStudents;
             $this->usersPerGroup['problemes'] = [];
-
             $this->counts = [
                 'pasMaj' => count($this->usersPerGroup['pasMaj']),
                 'update' => count($this->usersPerGroup['update']),
@@ -141,11 +124,9 @@ class StudentImportController {
             // echo 'updateRedoublants: '. count($this->usersPerGroup['updateRedoublants']).' // ';
             // echo 'nouveau: '. count($this->usersPerGroup['nouveau']).' // ';
             // pasMaj: 296 // update: 793 // updateRedoublants: 18 // nouveau: 282 //
-
             $fileName = 'uploads/etudiantsUpload'.date("Ymd").'.csv';
             $fp = fopen($fileName, 'w');
             fwrite($fp, "id_icam;login;nom;prenom;mail;promo;filiere;site;badge_uid;expiration_badge;naissance;sexe;img_link\n");
-
             $updateQuery = "";
             foreach ($this->usersPerGroup['update'] as $user) {
                 fwrite($fp, implode(";", array_values($user)) . "\n");
@@ -203,14 +184,12 @@ class StudentImportController {
                     $this->usersPerGroup['problemes'][$user['login']] = $user;
                     echo '<h4>Erreur avec <small>'.$user['login'].'</small></h4><p>'.$e->getMessage().'</p>';
                 }
-
             }
             foreach ($this->usersPerGroup['pasMaj'] as $user) {
                 fwrite($fp, implode(";", array_values($user)) . "\n");
             }
-
             fclose($fp);
         } else throw new \Exception("fichier inconnu");
     }
-
 }
+
